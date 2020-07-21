557ea183f438028ae7159e2301f851b832003c5b
Самусенко О.В <sov@dc.transoil.gomel.by>
2020-07-21 16:12:54 +0300

    version 2

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace UchetAuto.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("UchetAutoEntities", throwIfV1Schema: false)
         {
         }
 
